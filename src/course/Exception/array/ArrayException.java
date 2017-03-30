59cb8f9b78878a8adafa6db4151d309e71042a1e
Nagenderbabu <nagenderbabu34@gmail.com>
2017-03-30 16:50:45 +0530

    added comments

--- src/course/Exception/array/ArrayException.java
+++ src/course/Exception/array/ArrayException.java
@@ -2,16 +2,23 @@ package course.Exception.array;
 
 public class ArrayException {
 
+	// declaring array and i variable
 	int array[];
 	int i;
+	
+	// constructor with parameter
 	public ArrayException(int[] array, int i) {
 		super();
 		this.array = array;
 		this.i = i;
 	}
+	
+	// setters and getters
+	
 	public int[] getArray() {
 		return array;
 	}
+	
 	public void setArray(int[] array) {
 		this.array = array;
 	}
