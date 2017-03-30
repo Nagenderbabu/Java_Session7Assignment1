916030a7daed5ccc418fd2eafc1c6ec4d93f6734
Nagenderbabu <nagenderbabu34@gmail.com>
2017-03-30 16:51:53 +0530

    comments

--- src/course/Exception/array/Main.java
+++ src/course/Exception/array/Main.java
@@ -5,8 +5,10 @@ public class Main{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		// try block
+		// logic is placed in try block 
 		try{
-			
 		int array[]=new int[5];
 		ArrayException ae=new ArrayException(array, 5);
 		for(ae.i=0;ae.i<5;ae.i++)
@@ -16,8 +18,10 @@ public class Main{
 	    for(ae.i=0;ae.i<5;ae.i++){
 	    	System.out.println(array[ae.i]);
 	    	}
+	    // exception occurred
 	    System.out.println(array[ae.i+1]);
 }
+	//	catches Exception and prints message
 catch(Exception e){
 	System.out.println("Cannot call  value at index  out of array size ");
 }
